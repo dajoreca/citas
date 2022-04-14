37024a2e1b85b491af68bf1f41a311ab7b1f343d
Daniel Reollo <dreollo92@gmail.com>
2022-04-14 13:26:27 +0200

    Cambio Dia 1

--- App.js
+++ App.js
@@ -1,112 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState } from 'react'
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  StyleSheet,
+  Button,
+  Pressable,
+  Modal
+
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Formulario from './src/components/Formulario';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const App = () => {
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [modalVisible, setModalVisible] = useState (false) 
+    console.log(modalVisible)
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
+  const nuevaCitaHandler = () => {
+    console.log('diste click...')
+  }
+      //Esto es mejor que :
+      //<Pressable
+      //onPress={ () => {
+        //console.log('Presionaste el boton 1')
+      //}}
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style ={styles.container}>
+      <Text style={styles.titulo}> Administrador de Citas {' '} 
+        <Text style = {styles.tituloBold}>Veterinaria
+        </Text>
+      </Text>
+
+      <Pressable
+        onPress = { nuevaCitaHandler }
+      //para un codigo corto no esta mal, pero mejor sacarlo del return
+        //onPress={ () => {
+        // console.log('Presionaste el boton 1')
+        //}}
+
+        //onPressIn={ () => {
+        //  console.log('Presionaste el boton 2')
+        //}}
+
+        //onLongPress={ () => {
+        //  console.log('Presionaste el boton mas de la cuenta')
+        //}}
+
+        //onPressOut={ () => {
+        //  console.log('Dejaste de presionar el boton')
+        //}}
+        style={styles.btnNuevaCita}
+      >
+
+          <Text
+            style={styles.btnTextoNuevaCita}
+            onPress={() => setModalVisible(true)}
+          >Nueva Cita</Text>
+      </Pressable>
+
+      <Formulario
+        modalVisible={modalVisible}
+      
+      />
+    
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+
+  container: {
+    backgroundColor: '#77E6CB',
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  titulo: {
+    textAlign: 'center',
+    fontSize : 30,
+    color: '#374151',
+    fontWeight: '600'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
   },
-  highlight: {
-    fontWeight: '700',
+  btnNuevaCita: {
+    backgroundColor: '#30D915',
+    padding: 15,
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 10
   },
-});
+  btnTextoNuevaCita: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: '900',
+    textTransform: 'uppercase'
+  }
+
+})
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,147 @@
+
+
+import React, {useState} from 'react'
+import { Modal, Text, Button, SafeAreaView, StyleSheet, TextInput, View, ScrollView
+} from 'react-native'
+//import DatePicker from 'react-native-date-picker'
+
+const Formulario = ({modalVisible }) => {
+
+    const [paciente, setPaciente] = useState('')
+    const [propietario, setPropietario] = useState('')
+    const [email, setEmail] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [fecha, setFecha] = useState(new Date())
+    const [sintomas, setSintomas] = useState('')
+
+  return (
+    <Modal
+        animationType='slide'
+        visible={modalVisible}
+      >
+         <SafeAreaView style={styles.contenido}>
+            <ScrollView>
+            <Text
+                style={styles.titulo}
+            >Nueva {''}
+                <Text style={styles.tituloBold}
+                >Cita
+                </Text>
+            </Text>
+
+            <View style={styles.campo}>
+                    <Text style={styles.label}
+                    >Nombre Paciente
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Nombre Paciente'
+                        placeholderTextColor={'#666'}
+                        value={paciente}
+                        onChangeText= {setPaciente}
+                    />
+            </View>
+
+            <View style={styles.campo}>
+                    <Text style={styles.label}
+                    >Nombre Propietario
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Nombre Propietario'
+                        placeholderTextColor={'#666'}
+                        value={propietario}
+                        onChangeText= {setPropietario}
+                    />
+            </View>
+
+            <View style={styles.campo}>
+                    <Text style={styles.label}
+                    >Email Propietario
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Email Propietario'
+                        placeholderTextColor={'#666'}
+                        keyboardType= 'email-address'
+                        value={email}
+                        onChangeText= {setEmail}
+                    />
+            </View>
+
+            <View style={styles.campo}>
+                    <Text style={styles.label}
+                    >Telefono Propietario
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Telefono Propietario'
+                        placeholderTextColor={'#666'}
+                        keyboardType= 'number-pad'
+                        value={telefono}
+                        onChangeText= {setTelefono}
+                        maxLength= {9}
+                    />
+            </View>
+
+          
+            <View style={styles.campo}>
+                    <Text style={styles.label}
+                    >Síntomas
+                    </Text>
+                    <TextInput
+                        style={[styles.input, styles.sintomasImput]}
+                        placeholder='Sintomas del paciente'
+                        placeholderTextColor={'#666'}
+                        value={sintomas}
+                        onChangeText= {setSintomas}
+                        multiline={true}
+                        numberOfLines= {4}
+                    />
+            </View>
+            </ScrollView>
+         </SafeAreaView>
+      </Modal>
+     )
+}
+
+const styles = StyleSheet.create({
+
+    contenido: {
+        backgroundColor: '#6D28D9',
+        flex: 1,
+    },
+
+    titulo: {
+        fontSize: 30,
+        fontWeight: '600',
+        textAlign: 'center',
+        marginTop: 30,
+        color: '#FFF'
+    },
+    tituloBold: {
+        fontWeight: '900'
+    },
+    campo: {
+        marginTop: 10,
+        marginHorizontal: 30,
+    },
+    label: {
+        color: '#FFF',
+        marginBottom: 10,
+        marginTop: 15,
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    input:{
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius: 10,
+        textAlignVertical: 'top'
+    },
+    sintomasImput: {
+        height: 100
+    }
+})
+
+export default Formulario
